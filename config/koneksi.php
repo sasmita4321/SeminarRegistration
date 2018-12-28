bc84c824cbf05a3dfd789eba7bbbb6c47015dcdd
Mohamad Kadhafi <mokadhafi@gmail.com>
2018-12-28 10:37:30 +0700

    Fonts

--- config/koneksi.php
+++ config/koneksi.php
@@ -2,7 +2,11 @@
 $server = "localhost";
 $username = "root";
 $password = "";
+<<<<<<< Updated upstream
 $database = "db_semregist";
+=======
+$database = "SeminarRegistration";
+>>>>>>> Stashed changes
 
 // Koneksi dan memilih database di server
 mysql_connect($server,$username,$password) or die("Koneksi gagal");
